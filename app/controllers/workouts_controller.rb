d66e5d11a0eb4e961435b6582f605d8503e52e7b
Yoshi <yoshimejia00@gmail.com>
2021-10-26 19:20:21 -0400

    added create and update controller actions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,4 +3,28 @@ class WorkoutsController < ApplicationController
         workouts = Workout.all
         render json: workouts
     end
+
+    def create
+        workout = Workout.new(workout_params)
+        if workout.save
+            render json: workout
+        else
+            render json: {error: 'error'}
+        end 
+    end
+
+    def update
+        workout = Workout.find(params[:id])
+        if workout.update(workout_params)
+            render json: workout
+        else
+            render json: {error: 'error'}
+        end
+    end
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:name, :description, :sets, :reps, :completed, :planner_id)
+    end
 end
