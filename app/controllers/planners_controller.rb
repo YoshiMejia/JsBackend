9ca1f1b5004ecdf5b40afeb0daa764ed25b5d5f9
Yoshi <yoshimejia00@gmail.com>
2021-10-28 09:16:32 -0400

    added create action to planners controller

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -3,4 +3,18 @@ class PlannersController < ApplicationController
         planners = Planner.all
         render json: planners
     end
+
+    def create
+        planner = Planner.new(planner_params)
+        if planner.save
+            render json: planner
+        else
+            render json: {error: 'error'}
+        end
+    end
+
+    private
+    def planner_params
+        params.require(:planner).permit(:name)
+    end
 end
