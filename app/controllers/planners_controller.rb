91511578418957f0641adc263b7b6c60b30dbcf1
Yoshi <yoshimejia00@gmail.com>
2021-10-28 13:22:44 -0400

    added update action that also created new Workout for the specified Planner

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -13,8 +13,21 @@ class PlannersController < ApplicationController
         end
     end
 
+    def update
+        planner = Planner.find(params[:id])
+        workout = Workout.new(name: updated_planner_params[:workout_attributes][:name], description: updated_planner_params[:workout_attributes][:description], sets: updated_planner_params[:workout_attributes][:sets], reps: updated_planner_params[:workout_attributes][:reps], planner_id: params[:id])
+        if workout.save
+            render json: planner
+        else
+            render json: {error: 'error'}
+        end
+    end
+
     private
     def planner_params
-        params.require(:planner).permit(:name)
+        params.require(:planner).permit(:name, :id)
+    end
+    def updated_planner_params
+        params.require(:planner).permit(:name, :id, workout_attributes: [:name, :description, :sets, :reps])
     end
 end
